7e172c59091c7ff76651181e8444c46052444988
heynicortiz <ortiz.nagare@gmail.com>
2022-10-20 00:15:57 -0400

    initial commit

---
+++ app/src/main/java/com/example/unnamedgroup12project/adapters/MarketListingAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.unnamedgroup12project.adapters
+
+class MarketListingAdapter {
+}
---
+++ app/src/main/java/com/example/unnamedgroup12project/objects/Market.kt
@@ -0,0 +1,4 @@
+package com.example.unnamedgroup12project.objects
+
+class Market(val name: String, val address: String, val phone: String, val category: String) {
+}
