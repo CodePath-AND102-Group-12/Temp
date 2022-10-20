a8657d8deb0b6afe95562d14783adcfa40888214
heynicortiz <ortiz.nagare@gmail.com>
2022-10-19 21:58:54 -0400

    implemented replaceFragment() to improve readability and reduce redundant code

--- app/src/main/java/com/example/unnamedgroup12project/activities/MainActivity.kt
+++ app/src/main/java/com/example/unnamedgroup12project/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import com.example.unnamedgroup12project.ProjectViewModel
 import com.example.unnamedgroup12project.R
@@ -11,6 +13,16 @@ import com.example.unnamedgroup12project.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // fragment support, each fragment gets initialized
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    private val marketListFragment: Fragment = MarketListingFragment()
+    private val mapFragment: Fragment = MapFragment()
+    private val shoppingListFragment: Fragment = ShoppingListFragment()
+    private val calendarFragment: Fragment = CalendarFragment()
+    private val settingsFragment: Fragment = SettingsFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,80 +35,29 @@ class MainActivity : AppCompatActivity() {
 
 
         nav.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
             when (item.itemId) {
-                (R.id.action_goto_markets) -> {
-                    val supportFragmentManager = supportFragmentManager
-                    supportFragmentManager.commit {
-                        //TODO: Implement animations here!
-                        //setCustomAnimations(
-                        //    R.anim.fade_in,
-                        //   R.anim.slide_out
-                        //)
-                        replace(R.id.content, MarketListingFragment())
-                        addToBackStack(null)
-                    }
-                    true
-
-                }
-
-                (R.id.action_goto_map) -> {
-                    val supportFragmentManager = supportFragmentManager
-                    supportFragmentManager.commit {
-                        //TODO: Implement animations here!
-                        //setCustomAnimations(
-                        //    R.anim.fade_in,
-                        //   R.anim.slide_out
-                        //)
-                        replace(R.id.content, MapFragment())
-                        addToBackStack(null)
-                    }
-                    true
-                }
-
-                (R.id.action_goto_shopping_list) -> {
-                    val supportFragmentManager = supportFragmentManager
-                    supportFragmentManager.commit {
-                        //TODO: Implement animations here!
-                        //setCustomAnimations(
-                        //    R.anim.fade_in,
-                        //   R.anim.slide_out
-                        //)
-                        replace(R.id.content, ShoppingListFragment())
-                        addToBackStack(null)
-                    }
-                    true
-                }
-
-                (R.id.action_goto_calendar) -> {
-                    val supportFragmentManager = supportFragmentManager
-                    supportFragmentManager.commit {
-                        //TODO: Implement animations here!
-                        //setCustomAnimations(
-                        //    R.anim.fade_in,
-                        //   R.anim.slide_out
-                        //)
-                        replace(R.id.content, CalendarFragment())
-                        addToBackStack(null)
-                    }
-                    true
-                }
-
-                (R.id.action_goto_settings) -> {
-                    val supportFragmentManager = supportFragmentManager
-                    supportFragmentManager.commit {
-                        //TODO: Implement animations here!
-                        //setCustomAnimations(
-                        //    R.anim.fade_in,
-                        //   R.anim.slide_out
-                        //)
-                        replace(R.id.content, SettingsFragment())
-                        addToBackStack(null)
-                    }
-                    true
-                }
-                else ->
-                    true
+                R.id.action_goto_markets -> fragment = marketListFragment
+                R.id.action_goto_map -> fragment = mapFragment
+                R.id.action_goto_shopping_list -> fragment = shoppingListFragment
+                R.id.action_goto_calendar -> fragment = calendarFragment
+                R.id.action_goto_settings -> fragment = settingsFragment
             }
+            replaceFragment(fragment)
+            true
         }
+
+        // set default as Market Listing fragment
+        nav.selectedItemId = R.id.action_goto_markets
+
+        // start the Activity on Market Listing fragment
+        replaceFragment(marketListFragment)
+    }
+
+    // Helper function to clean up fragment changes
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, fragment)
+        fragmentTransaction.commit()
     }
 }
