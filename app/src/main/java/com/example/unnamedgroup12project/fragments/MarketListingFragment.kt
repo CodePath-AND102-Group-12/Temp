390fdf7f9906deda368fde84933ce81ab27a2dd4
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-20 22:53:31 -0400

    switched All Markets RV to Grid Layout

--- app/src/main/java/com/example/unnamedgroup12project/fragments/MarketListingFragment.kt
+++ app/src/main/java/com/example/unnamedgroup12project/fragments/MarketListingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.unnamedgroup12project.R
@@ -42,7 +43,7 @@ class MarketListingFragment : Fragment() {
         featuredMarketListRecyclerView.adapter = featuredMarketListingAdapter
         featuredMarketListRecyclerView.layoutManager = featuredMarketListlayoutManager
 
-        val allMarketListLayoutManager = LinearLayoutManager(context)
+        val allMarketListLayoutManager = GridLayoutManager(context, 2)
         val allMarketListRecyclerView = view.findViewById<RecyclerView>(R.id.allMarketsRecyclerView)
         allMarketListingAdapter = MarketListingAdapter(marketList)
 
