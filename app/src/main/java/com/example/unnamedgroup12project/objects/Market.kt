7ae754b8a5e43102bf640f13a4bb0bd673d44d67
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-20 22:52:07 -0400

    added image string to constructor

--- app/src/main/java/com/example/unnamedgroup12project/objects/Market.kt
+++ app/src/main/java/com/example/unnamedgroup12project/objects/Market.kt
@@ -1,4 +1,4 @@
 package com.example.unnamedgroup12project.objects
 
-class Market(val name: String, val address: String, val phone: String, val category: String) {
+class Market(val name: String, val address: String, val phone: String, val category: String, val image: String) {
 }
