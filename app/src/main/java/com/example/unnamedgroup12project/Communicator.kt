b42bcd40e99055ff7d4761a3a92ad0a880de5c2a
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-22 12:39:44 -0400

    initial commit - assists with passing Market details to detail fragment

---
+++ app/src/main/java/com/example/unnamedgroup12project/Communicator.kt
@@ -0,0 +1,6 @@
+package com.example.unnamedgroup12project
+
+interface Communicator {
+    fun passDataCom(toPass: ArrayList<String>)
+    // from this article: https://johncodeos.com/how-to-pass-data-between-fragments-in-android-using-kotlin/
+}
