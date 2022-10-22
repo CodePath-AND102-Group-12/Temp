86e609e917c5cb9265dcf0c2b6b26985cc7db20a
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-22 12:41:01 -0400

    onClick now working to pass data to MarketDetailFragment

--- app/src/main/java/com/example/unnamedgroup12project/adapters/MarketListingAdapter.kt
+++ app/src/main/java/com/example/unnamedgroup12project/adapters/MarketListingAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.unnamedgroup12project.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.unnamedgroup12project.Communicator
 import com.example.unnamedgroup12project.R
 import com.example.unnamedgroup12project.objects.Market
 
@@ -17,7 +19,7 @@ private lateinit var  marketName: TextView
 private lateinit var  marketCategory: TextView
 private lateinit var marketImage: ImageView
 
-class MarketListingAdapter(private val marketList: MutableList<Market>): RecyclerView.Adapter<MarketListingAdapter.ViewHolder>() {
+class MarketListingAdapter(private val marketList: MutableList<Market>, private val context: Context, private val activity: AppCompatActivity, private val comm: Communicator): RecyclerView.Adapter<MarketListingAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -46,21 +48,35 @@ class MarketListingAdapter(private val marketList: MutableList<Market>): Recycle
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val market = marketList.get(position)
+        val market = marketList[position]
 
         marketName.text = market.name
         marketCategory.text = market.category
 
-        allMarketsRVLayout.setOnClickListener {
-            Toast.makeText(it.context, "${market.name} clicked", Toast.LENGTH_SHORT).show()
-        }
-
-
+        // This should be replaced by whatever the actual image URL is instead of these generic images
         when (market.image) {
             "produce" -> marketImage.setImageResource(R.drawable.produce)
             "bakery" -> marketImage.setImageResource(R.drawable.bakery)
             "fruits" -> marketImage.setImageResource(R.drawable.fruits)
             "plants" -> marketImage.setImageResource(R.drawable.plants)
+        }
+
+        allMarketsRVLayout.setOnClickListener {
+            Toast.makeText(it.context, "${market.name} clicked", Toast.LENGTH_SHORT).show()
+
+            val newMarket = marketList[holder.layoutPosition]
+            val marketArrayList = arrayListOf(newMarket.name, newMarket.address, newMarket.phone, newMarket.category, newMarket.image)
+
+            // communicator to carry information to new fragment
+            comm.passDataCom(marketArrayList)
+
+//            val transaction = activity.supportFragmentManager.beginTransaction()
+//            val detailFragment = MarketDetailFragment()
+//            transaction.replace(R.id.content, detailFragment)
+//            transaction.addToBackStack("this fragment")
+//            transaction.commit()
+
+
         }
     }
 
