40d68bcd5decb4cfd09b073341fc0b5339ba0e49
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-22 12:40:32 -0400

    working concept now

--- app/src/main/java/com/example/unnamedgroup12project/fragments/MarketDetailFragment.kt
+++ app/src/main/java/com/example/unnamedgroup12project/fragments/MarketDetailFragment.kt
@@ -1,13 +1,24 @@
 package com.example.unnamedgroup12project.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.unnamedgroup12project.R
+import com.example.unnamedgroup12project.objects.Market
 
 class MarketDetailFragment : Fragment() {
+
+    private lateinit var marketDetailDataArray: ArrayList<String>
+    private lateinit var marketDetail: Market
+    private lateinit var marketImage: ImageView
+    private lateinit var marketName: TextView
+    private lateinit var marketAddress: TextView
+    private lateinit var marketCategory: TextView
+    private lateinit var marketPhone: TextView
 
 
     override fun onCreateView(
@@ -15,6 +26,37 @@ class MarketDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_market_detail, container, false)
+
+        marketImage = view.findViewById(R.id.marketImage)
+        marketName = view.findViewById(R.id.marketName)
+        marketAddress = view.findViewById(R.id.marketAddress)
+        marketCategory = view.findViewById(R.id.marketCategory)
+        marketPhone = view.findViewById(R.id.marketPhone)
+
+        // recreate array of market data from passed in argument
+        marketDetailDataArray = arguments?.getStringArrayList("input_txt") as ArrayList<String>
+
+        // create market object based on the data array
+        marketDetail = Market(marketDetailDataArray[0], marketDetailDataArray[1], marketDetailDataArray[2], marketDetailDataArray[3], marketDetailDataArray[4])
+
+        // use that data to display in the fragment
+        marketName.text = marketDetail.name
+        marketAddress.text = marketDetail.address
+        marketCategory.text = marketDetail.category
+        marketPhone.text = marketDetail.phone
+
+        // This should be replaced by whatever the actual image URL is instead of these generic images
+        when (marketDetail.image) {
+            "produce" -> marketImage.setImageResource(R.drawable.produce)
+            "bakery" -> marketImage.setImageResource(R.drawable.bakery)
+            "fruits" -> marketImage.setImageResource(R.drawable.fruits)
+            "plants" -> marketImage.setImageResource(R.drawable.plants)
+        }
+
         return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 }
