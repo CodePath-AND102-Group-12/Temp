a3ebe130389c9580d6d4b190ac8ff205953f3b52
Coreyander <coalex89@gmail.com>
2022-10-25 16:12:31 -0400

    Commit for 10/25/2022 changes - implemented MVVM architecture to move data - fixed data leaks in MarketListingAdapter - replaced ArrayList arg with Market object

--- app/src/main/java/com/example/unnamedgroup12project/fragments/MarketDetailFragment.kt
+++ app/src/main/java/com/example/unnamedgroup12project/fragments/MarketDetailFragment.kt
@@ -7,18 +7,21 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.unnamedgroup12project.ProjectViewModel
 import com.example.unnamedgroup12project.R
 import com.example.unnamedgroup12project.objects.Market
 
-class MarketDetailFragment : Fragment() {
+class MarketDetailFragment() : Fragment() {
 
-    private lateinit var marketDetailDataArray: ArrayList<String>
-    private lateinit var marketDetail: Market
+    //private lateinit var marketDetailDataArray: ArrayList<String>
     private lateinit var marketImage: ImageView
     private lateinit var marketName: TextView
     private lateinit var marketAddress: TextView
     private lateinit var marketCategory: TextView
     private lateinit var marketPhone: TextView
+
+    private val viewModel: ProjectViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -34,19 +37,19 @@ class MarketDetailFragment : Fragment() {
         marketPhone = view.findViewById(R.id.marketPhone)
 
         // recreate array of market data from passed in argument
-        marketDetailDataArray = arguments?.getStringArrayList("input_txt") as ArrayList<String>
+        //marketDetailDataArray = arguments?.getStringArrayList("input_txt") as ArrayList<String>
 
         // create market object based on the data array
-        marketDetail = Market(marketDetailDataArray[0], marketDetailDataArray[1], marketDetailDataArray[2], marketDetailDataArray[3], marketDetailDataArray[4])
+        val marketDetail = viewModel.market.value
 
         // use that data to display in the fragment
-        marketName.text = marketDetail.name
-        marketAddress.text = marketDetail.address
-        marketCategory.text = marketDetail.category
-        marketPhone.text = marketDetail.phone
+        marketName.text = marketDetail?.name
+        marketAddress.text = marketDetail?.address
+        marketCategory.text = marketDetail?.category
+        marketPhone.text = marketDetail?.phone
 
         // This should be replaced by whatever the actual image URL is instead of these generic images
-        when (marketDetail.image) {
+        when (marketDetail?.image) {
             "produce" -> marketImage.setImageResource(R.drawable.produce)
             "bakery" -> marketImage.setImageResource(R.drawable.bakery)
             "fruits" -> marketImage.setImageResource(R.drawable.fruits)
@@ -56,7 +59,4 @@ class MarketDetailFragment : Fragment() {
         return view
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
