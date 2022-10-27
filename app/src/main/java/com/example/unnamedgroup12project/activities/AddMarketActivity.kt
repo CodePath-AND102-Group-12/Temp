81655d2976cd43ff2a65636a464663b0f28dfd54
Kenneth Ng <kng11@wgu.edu>
2022-10-27 19:27:44 -0400

    Added the image upload function. Name of image will be same name as the node of the market (which is the name of the market).

--- app/src/main/java/com/example/unnamedgroup12project/activities/AddMarketActivity.kt
+++ app/src/main/java/com/example/unnamedgroup12project/activities/AddMarketActivity.kt
@@ -15,6 +15,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.net.URI
 
 
 class AddMarketActivity : AppCompatActivity() {
@@ -25,6 +26,8 @@ class AddMarketActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
     private lateinit var databaseReference: DatabaseReference
     private lateinit var storageReference: StorageReference
+
+    /** pointer to location of image**/
     private lateinit var imageUri: Uri
 
     private lateinit var dialog: Dialog
@@ -43,8 +46,13 @@ class AddMarketActivity : AppCompatActivity() {
         /** References the node to which market data is stored**/
         databaseReference = FirebaseDatabase.getInstance().getReference("Markets")
 
+        /** Upload image button chooses 1 picture from your gallery**/
+        binding.marketUploadImageBtn.setOnClickListener {
+            selectImage()
+        }
 
-        /** Cancel button intents to main listing**/
+
+        /** Cancel button intents into main listing**/
         binding.marketCancelBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             this.startActivity(intent)
@@ -75,20 +83,20 @@ class AddMarketActivity : AppCompatActivity() {
                 marketDescription,
                 marketDateTime)
 
+            /** Creates the entity(child) into the node based on the market name**/
             databaseReference.child(marketName).setValue(markets).addOnCompleteListener {
                 if (it.isSuccessful){
-                    uploadPic()
+                    uploadImage()
                     hideProgressBar()
 
+                    /**Go back to main screen**/
                     val intent = Intent(this, MainActivity::class.java)
                     this.startActivity(intent)
 
                 }else{
-
                     hideProgressBar()
                     Toast.makeText(this@AddMarketActivity,"Failed to list your market",Toast.LENGTH_SHORT).show()
                 }
-
             }
 
 /** temporary because no authentication implemented yet**/
@@ -117,11 +125,40 @@ class AddMarketActivity : AppCompatActivity() {
 
     }
 
-    private fun uploadPic() {
-        imageUri = Uri.parse("android.resource://$packageName/${R.drawable.fish}")
-        //storageReference = FirebaseStorage.getInstance().getReference("Markets/"+auth.currentUser?.uid)
-        storageReference = FirebaseStorage.getInstance().getReference("Markets/temp")
+    /** Function intents into image gallery**/
+    private fun selectImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 100)
+    }
 
+
+    /** Function for when image selected from gallery
+        Shows the image in the imageView
+        Assigns pointer imageUri the image **/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100 && resultCode == RESULT_OK){
+
+            imageUri = data?.data!!
+            binding.marketPictureIV.setImageURI(imageUri)
+
+        }
+    }
+
+    private fun uploadImage() {
+        val marketName = binding.marketNameET.text.toString()
+
+
+        // imageUri = Uri.parse("android.resource://$packageName/${R.drawable.fish}")
+        // storageReference = FirebaseStorage.getInstance().getReference("Markets/"+auth.currentUser?.uid)
+
+        /** References where to store the image**/
+        storageReference = FirebaseStorage.getInstance().getReference("Markets/$marketName")
+
+        /** putFile() stores in firebase **/
         storageReference.putFile(imageUri).addOnSuccessListener {
             Toast.makeText(this@AddMarketActivity,"Image upload Successful",Toast.LENGTH_LONG).show()
         }.addOnFailureListener {
